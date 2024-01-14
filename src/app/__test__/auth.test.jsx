ecfb98ef04274f58a9729ee8a7d0cbb90076f4bf
Hà Công Bằng <hacongbang174@gmail.com>
2024-01-14 21:00:04 +0700

    Update base test

--- src/app/__test__/auth.test.jsx
+++ src/app/__test__/auth.test.jsx
@@ -1,111 +1,55 @@
-import authAPI from '../api/auth';
-import {AUTH} from '../constants/global'
-import { AuthenticationError, DefaultError, BadRequestError, NotFoundError } from '../constants/errors';
-jest.mock('axios');
-import axios from 'axios';
+const required = require('supertest')
+let access_token = '';
 
 describe('Login tests', () => {
-  describe('login function', () => {
-    const body = {
-      phone : '0932323232',
-      password : 'Abc@123456'
-    }
-    describe('with success', () => {
-      const data = {something: {}};
+  const body = {
+    phone: '0932323232',
+    password: 'Abc@123456'
+  }
+  it("should call endpoint with phone & password true, return 201", async () => {
+    const response = await required(`https://gce.onedev.top/api/v1/auth/sign-in`).post("").send(body);
+    access_token = response._body.access_token
+    expect(response.statusCode).toBe(201);
+  })
 
-      beforeEach(() => {
-        axios.post.mockResolvedValue({ data });
-      });
+  it("should call endpoint with phone & password false, return 401", async () => {
+    const response = await required(`https://gce.onedev.top/api/v1/auth/sign-in`).post("").send(body);
+    expect(response.statusCode).toBe(401);
+  })
 
-      it('should call endpoint with given phone & password', async () => {
-        await authAPI.login(body);
-        expect(axios.post).toBeCalledWith(
-          `${AUTH.LOGIN}`,
-          body,
-        );
-      });
-
-      it('should return response data', async () => {
-        const response = await authAPI.login(body);
-        expect(response).toStrictEqual(data);
-      });
-    });
-
-    describe('with error', () => {
-      describe('status 401', () => {
-        beforeEach(() => {
-          axios.post.mockRejectedValue({ status: 401 });
-        });
-
-        it('should throw AuthenticationError', async () => {
-          await expect(authAPI.login(body)).rejects.toThrow(AuthenticationError);
-        });
-      });
-
-      describe('other status', () => {
-        beforeEach(() => {
-          axios.post.mockRejectedValue({});
-        });
-
-        it('should throw DefaultError', async () => {
-          await expect(authAPI.login(body)).rejects.toThrow(DefaultError);
-        });
-      });
-    });
-  });
 });
 
-describe('Register tests', () => {
-  describe('register function', () => {
-    const body = {
-      username: 'Test API',
-      phone : '0932323232',
-      email: 'testapi1@gmail.com',
-      password : 'Abc@123456'
-    }
 
+describe('Create product tests',  () => {
 
-    describe('with success', () => {
-      const data = {something: {}};
+  const body = {
+    name: "Laptop MSI Gaming Raider GE77HX 12UHS-229VN i9 12900HX/64GB/2TB/17.3UHD/Nvidia RTX 3080Ti 16GB/Win11",
+    description: "Siêu máy tính MSI Gaming Raider GE77HX 12UHS-229VN đích thực là một con quái vật cấu hình với những thông số mạnh nhất có thể đi cùng một thiết kế đẹp khó cưỡng. Hãy cùng xem bộ vi xử lý Intel Core i9 12900HX và card đồ họa RTX 3080 Ti có thể mang đến những trải nghiệm đỉnh cao như thế nào.",
+    images: [
+      {
+        name: "14",
+        image_url: "https://gce.onedev.top/uploads/a838d2f3310b06ccdf24ee238bbb3b8c9.jpg",
+        resource_type: "products",
+        resource_id: "",
+        description: ""
+      }
+    ],
+    price: 126990000,
+    amount: 88893000,
+    percent_discount: 30,
+    product_category_id: "a7555d82-b4d1-41d6-bb4b-76d5ed516699",
+    user_id: "7cf56e74-4d72-4465-a005-7ad22fcaf229",
+  }
 
-      beforeEach(() => {
-        axios.post.mockResolvedValue({ data });
-      });
-
-      it('should call endpoint with body', async () => {
-        await authAPI.register(body);
-        expect(axios.post).toBeCalledWith(
-          `${AUTH.REGISTER}`,
-          body,
-        );
-      });
-
-      it('should return response data', async () => {
-        const response = await authAPI.register(body);
-        expect(response).toStrictEqual(data);
-      });
-    });
-
-    describe('with error', () => {
-      describe('status 400', () => {
-        beforeEach(() => {
-          axios.post.mockRejectedValue({ status: 400 });
-        });
-
-        it('should throw BadRequestError', async () => {
-          await expect(authAPI.register(body)).rejects.toThrow(BadRequestError);
-        });
-      });
-      
-      describe('other status', () => {
-        beforeEach(() => {
-          axios.post.mockRejectedValue({});
-        });
-
-        it('should throw DefaultError', async () => {
-          await expect(authAPI.register(body)).rejects.toThrow(DefaultError);
-        });
-      });
-    });
-  });
+    it("should return 200", async () => {
+      const response = await required(`https://gce.onedev.top/api/v1/e-commerce/products`).post("").send(body).set('Authorization', `Bearer ${access_token}`);
+      access_token = response._body.access_token
+      expect(response.statusCode).toBe(201);
+    })
+    it("should return 400", async () => {
+      const response = await required(`https://gce.onedev.top/api/v1/e-commerce/products`).post("").send(body).set('Authorization', `Bearer ${TOKEN}`);
+      access_token = response._body.access_token
+      expect(response.statusCode).toBe(400);
+    })
 });
+
--- src/app/constants/global.js
+++
@@ -1,6 +0,0 @@
-const SERVER_API = "https://gce.onedev.top/api/v1";
-
-export const AUTH = {
-  LOGIN: `${SERVER_API}/auth/sign-in`,
-  REGISTER: `${SERVER_API}/auth/register`,
-};
